51c2764219406b5ae69026c2bb3ae89571c76f4f
WallochMatt <matthewrwalloch@gmail.com>
2022-10-24 14:23:16 -0500

    feat: Set up axios get request + Created songs table

---
+++ src/Components/DisplayMusic/DisplayMusic.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const DisplayMusic = (props) => {
+
+    useEffect(()=> {
+    getAllSongs();
+    }, [])
+
+
+    async function getAllSongs(){
+        let response = await axios.get('http://127.0.0.1:8000/api/music/');
+        props.setList(response.data);
+        }
+    
+    
+        return(
+        <table>
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Album</th>
+                    <th>Artist</th>
+                    <th>Genre</th>
+                    <th>Release Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.list.map((song) => {
+                    return(
+                        <tr>
+                            <td>{song.title}</td>
+                            <td>{song.artist}</td>
+                            <td>{song.album}</td>
+                            <td>{song.genre}</td>
+                            <td>{song.release_date}</td>
+                        </tr>
+                            )
+                    })
+                }
+            </tbody>
+        </table>
+    )
+}
+
+export default DisplayMusic;
