6a0c51555ba2938268c26f7fd8e864be37b0da48
WallochMatt <matthewrwalloch@gmail.com>
2022-10-25 12:58:35 -0500

    refactor: Converted SearchBar form prompt to form

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,44 +1,49 @@
+import { useState } from "react";
 
 const SearchBar = (props) => {
 
     let songsFound = props.list;
-    let results;
+    
+    const [search, setSearch] = useState([])
 
-    function filterSongs(){
-        console.log("in function");
-        
+    function filterSongs(criteria){
+        let results;
 
         try{ 
-            let search = prompt("Search database");
-            
             results = songsFound.filter(function(song){
-                if(song.title.toLowerCase() === search.toLowerCase()){
-                    console.log("in try");
+                if(song.title.toLowerCase() === criteria.toLowerCase()){
+                    // console.log("in try");
                     return true
                 }
                 return results
             });
         }
-
         catch(err){
-            console.log(err);
+            // console.log(err);
             results = props.list;
+            return results
         }
-
-        return (
+        return(
             console.log(results)
-
         )
-    
+    }
+
+    function handleSubmit(event){
+        // event.preventDefault();
+        filterSongs(search)
     }
 
     return(
-    <div>
-        <button onClick={() => filterSongs()}>SEARCH</button>
-        
-    </div>
-    )
+        <form onSubmit={handleSubmit()}>
+            <div>
 
+                <label>Search</label>
+                <input value={search} onChange={(event) => setSearch(event.target.value)}></input>
+                {/* <button onClick={() => filterSongs()}>SEARCH</button>
+         */}
+            </div>
+        </form>
+    )
 }
 
 export default SearchBar;
